2f9228a9b2dc2f8c78a9f6bd31b624bc4d7f3c81
Vivek Shrama <vsharma87077@gmail.com>
2024-02-19 12:42:44 +0530

    done

--- Topic Code/3 Data Types Variables and Input/UserInput.java
+++ Topic Code/3 Data Types Variables and Input/UserInput.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class UserInput {
+    @SuppressWarnings("resource")
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Please enter your name: ");
